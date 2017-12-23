cf3705c246e57d312559a08670fe342770295a2d
Jeff Trawick <trawick@gmail.com>
2017-12-23 10:02:31 -0500

    Initializing the repo

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+import e_contact
+
+VERSION = e_contact.__version__
+
+if sys.argv[-1] == 'tag':
+    os.system("git tag -a %s -m 'version %s'" % (VERSION, VERSION))
+    os.system("git push --tags")
+    sys.exit()
+
+setup(
+    name='emptyhammock_simple_plugins',
+    packages=find_packages(),
+    include_package_data=True,
+    license='Apache 2.0 License',
+    version=VERSION,
+    description='A Django app providing contact form support',
+    author='Emptyhammock Software and Services LLC',
+    author_email='info@emptyhammock.com',
+    url='https://github.com/trawick/emptyhammock-contact',
+    keywords=['django', 'cms'],
+    classifiers=[
+        'License :: OSI Approved :: Apache 2.0 License',
+        'Development Status :: 3 - Alpha',
+    ],
+)
