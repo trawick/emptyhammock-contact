b2933fb2993caf044ed00ae3393d53c29c0d45e5
Jeff Trawick <trawick@gmail.com>
2017-12-28 09:53:07 -0500

    Fix setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.argv[-1] == 'tag':
     sys.exit()
 
 setup(
-    name='emptyhammock_simple_plugins',
+    name='emptyhammock_contact',
     packages=find_packages(),
     include_package_data=True,
     license='Apache 2.0 License',
