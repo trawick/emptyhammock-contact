0398db3b86e1fde610ee5b9907be8bb6fef4d031
Jeff Trawick <trawick@gmail.com>
2018-01-26 16:30:33 -0500

    State field can now be edited in admin

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,9 @@
 # Changes and migration requirements
 
+## Version 0.0.5
+
+* Contact's `state` field is now editable in admin.
+
 ## Version 0.0.4
 
 * Fix show-stopper bug in reCAPTCHA support.
--- e_contact/admin.py
+++ e_contact/admin.py
@@ -7,9 +7,9 @@ class ContactAdmin(admin.ModelAdmin):
     list_filter = ('state',)
     search_fields = ('name', 'email', 'message',)
     readonly_fields = (
-        'state', 'name', 'email', 'message', 'created_at', 'modified_at', 'uuid',
+        'name', 'email', 'message', 'created_at', 'modified_at', 'uuid',
     )
-    fields = readonly_fields[:4] + ('notes',) + readonly_fields[4:]
+    fields = ('state',) + readonly_fields[:3] + ('notes',) + readonly_fields[3:]
 
 
 admin.site.register(models.Contact, ContactAdmin)
